a15a029b147f1322201b209ecf75e1b90215d252
jonnycat <82131956+McJonnycat@users.noreply.github.com>
2022-05-07 23:01:47 -0700

    Update VignetteEffect.hx

--- ZackGamerz Shaders/VignetteEffect.hx
+++ ZackGamerz Shaders/VignetteEffect.hx
@@ -1,6 +1,18 @@
 package;
 
 import flixel.system.FlxAssets.FlxShader;
+import openfl.display.BitmapData;
+import openfl.display.ShaderInput;
+import openfl.utils.Assets;
+import flixel.FlxG;
+import openfl.Lib;
+import flixel.math.FlxPoint;
+
+using StringTools;
+typedef ShaderEffect = {
+  var shader:Dynamic;
+} 
+	
 /**
  * ported by luckydog
  * helped? by zack
