4261993edd88f7fbf31c4ed622a8c7f9487e3a82
jonnycat <82131956+McJonnycat@users.noreply.github.com>
2022-05-14 22:33:01 -0700

    hehe among us lol

--- Jonnycat Shaders/WavyEffect.hx
+++ Jonnycat Shaders/WavyEffect.hx
@@ -13,52 +13,42 @@ typedef ShaderEffect = {
   var shader:Dynamic;
 } 
   
-class WavyEffect 
+class SurfEffect 
 { 
-public var shader(default, null):WavyShader = new WavyShader(); 
+public var shader(default, null):SurfShader = new SurfShader(); 
 
 public function new()
 {  
   shader.iTime.value = [0];
-  shader.iResolution.value = [Lib.current.stage.stageWidth,Lib.current.stage.stageHeight]; 
-  shader.frequency.value = [8.0];  
-  shader.data.amplitude.value = [0.05]; 
+  shader.data.amplitude.value = [0.10]; 
 } 
 
 public function update(elapsed:Float){
     shader.iTime.value[0] += elapsed;
-    shader.iResolution.value = [Lib.current.stage.stageWidth,Lib.current.stage.stageHeight]; 
 }  
-public function setfrequency(chickennuggets:Float){
-  shader.frequency.value = [chickennuggets];
-}  
-public function setamplitude(goof:Float){
-  shader.amplitude.value = [goof];
+public function setamplitude(goofy:Float){
+  shader.amplitude.value = [goofy];
 } 
 }
-class WavyShader extends FlxShader
+class SurfShader extends FlxShader
 { 
 @:glFragmentSource('
-  #pragma header
-  uniform vec2 iResolution; 
-  uniform float iTime;  
-  uniform float frequency; 
-  uniform float amplitude;
+  #pragma header 
+  uniform float iTime; 
+  unifrom float amplitude;
   void main()
-  {
-      vec2 fragCoord = openfl_TextureCoordv * iResolution;  
-
-      vec2 texCoord = fragCoord.xy / iResolution.xy; 
-      
-      vec2 pulse = sin(iTime - frequency * texCoord);
-      float dist = 2.0 * length(texCoord.y - 0.5);
-      
-      vec2 newCoord = texCoord + amplitude * vec2(0.0, pulse.x); // y-axis only; 
-      
-      vec2 interpCoord = mix(newCoord, texCoord, dist);
+{
+    vec2 texCoord = openfl_TextureCoordv;
     
-      gl_FragColor = flixel_texture2D(bitmap,interpCoord);
-  }
+    vec2 pulse = sin(iTime + texCoord);
+    float dist = 2.0 * length(texCoord.y - 0.5);
+    
+    vec2 newCoord = texCoord + amplitude * vec2(0.0, pulse.x); // y-axis only; 
+    
+    vec2 interpCoord = mix(newCoord, texCoord, dist);
+	
+	  gl_fragColor = flixel_texture2D(bitmap, interpCoord);
+}
   ')
   public function new()
   {
