d3a0944d200a98c568a16e6addd95b974b090e47
jonnycat <82131956+McJonnycat@users.noreply.github.com>
2022-05-20 18:44:47 -0700

    Update NoteEffect.hx

--- Nebulazorua Shaders/NoteEffect.hx
+++ Nebulazorua Shaders/NoteEffect.hx
@@ -37,7 +37,7 @@ class NoteShader extends FlxShader
 
     void main()
     {
-        vec4 col = flixel_texture2D(bitmap, openfl_TextureCoordv);
+        vec4 col = texture2D(bitmap, openfl_TextureCoordv);
         vec4 newCol = col;
         if(flash!=0.0 && col.a>0.0)
           newCol = mix(col,vec4(1.0,1.0,1.0,col.a),flash) * col.a;
